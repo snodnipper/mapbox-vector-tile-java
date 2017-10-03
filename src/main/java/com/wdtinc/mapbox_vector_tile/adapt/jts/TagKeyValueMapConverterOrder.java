8d8965cba854850cad929362704d1499059c7aca
Oliver Snowden <oliversnowden@snodnipper.co.uk>
2017-10-03 17:23:07 +0100

    Ordered Key Value

---
+++ src/main/java/com/wdtinc/mapbox_vector_tile/adapt/jts/TagKeyValueMapConverterOrder.java
@@ -0,0 +1,98 @@
+package com.wdtinc.mapbox_vector_tile.adapt.jts;
+
+import com.google.protobuf.ProtocolStringList;
+import com.wdtinc.mapbox_vector_tile.VectorTile;
+import com.wdtinc.mapbox_vector_tile.encoding.MvtValue;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Convert MVT tags list to a {@link Map} of {@link String} to {@link Object}. Tags indices that are out
+ * of range of the key or value list are ignored.
+ *
+ * @see ITagConverter
+ */
+public final class TagKeyValueMapConverterOrder implements ITagConverter {
+
+    /** If true, return null user data when tags are empty */
+    private final boolean nullIfEmpty;
+
+    /** If true, add id to user data object */
+    private final boolean addId;
+
+    /** The {@link Map} key for the feature id. */
+    private final String idKey;
+
+    /**
+     * Always created user data object, even with empty tags. Ignore feature ids.
+     */
+    public TagKeyValueMapConverterOrder() {
+        this(false);
+    }
+
+    /**
+     * Ignore feature ids.
+     *
+     * @param nullIfEmpty if true, return null user data when tags are empty
+     */
+    public TagKeyValueMapConverterOrder(boolean nullIfEmpty) {
+        this.nullIfEmpty = nullIfEmpty;
+        this.addId = false;
+        this.idKey = null;
+    }
+
+    /**
+     * Store feature ids using idKey. Id value may be null if not present.
+     *
+     * @param nullIfEmpty if true, return null user data when tags are empty
+     * @param idKey key name to use for feature id value
+     */
+    public TagKeyValueMapConverterOrder(boolean nullIfEmpty, String idKey) {
+        Objects.requireNonNull(idKey);
+
+        this.nullIfEmpty = nullIfEmpty;
+        this.addId = true;
+        this.idKey = idKey;
+    }
+
+    @Override
+    public Object toUserData(Long id, List<Integer> tags, ProtocolStringList keysList,
+                             List<VectorTile.Tile.Value> valuesList) {
+
+        // Guard: empty
+        if(nullIfEmpty && tags.size() < 1 && (!addId || id == null)) {
+            return null;
+        }
+
+
+        final Map<String, Object> userData = new LinkedHashMap<>(((tags.size() + 1) / 2));
+
+        // Add feature properties
+        int keyIndex;
+        int valIndex;
+        boolean valid;
+
+        for(int i = 0; i < tags.size() - 1; i += 2) {
+            keyIndex = tags.get(i);
+            valIndex = tags.get(i + 1);
+
+            valid = keyIndex >= 0 && keyIndex < keysList.size()
+                    && valIndex >= 0 && valIndex < valuesList.size();
+
+            if(valid) {
+                userData.put(keysList.get(keyIndex), MvtValue.toObject(valuesList.get(valIndex)));
+            }
+        }
+
+        // Add ID, value may be null
+        if(addId) {
+            userData.put(idKey, id);
+        }
+
+        return userData;
+    }
+}
